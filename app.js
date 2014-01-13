20dd6710c384b09e76ba2ac81390b478460a914a
George T. Walters II <walterg2@gmail.com>
2014-01-12 19:27:07 -0500

    Adding in some code to make things work a bit nicer; Counties table now holds current emergency level, who reported and when

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 Counties = new Meteor.Collection('counties');
-Reports = new Meteor.Collection('reports');
 Snowlevels = new Meteor.Collection('snowlevels');
 
 if (Meteor.isClient) {
@@ -12,19 +11,42 @@ if (Meteor.isClient) {
     return "Getting to SnowMash couldn't be more fun!";
   };
 
+  Template.counties.counties = function() {
+    return Counties.find({}, { sort: ['name', 'asc'] });
+  };
+
+  Template.snowlevels.snowlevels = function() {
+    return Snowlevels.find({}, { sort: ['level', 'desc'] });
+  };
+
   Template.login.events = {
     'click #signIn' : function() {
       Meteor.loginWithTwitter();
     }
   };
 
-  Template.counties.counties = function() {
-    return Counties.find({}, { sort: ['name', 'asc'] });   
+  Template.snowlevels.events = {
+    'change': function(e, tmpl) {
+      e.preventDefault();
+
+      var reportDate = new Date();
+
+      // This isn't working yet... Must learn all the things
+      if (Meteor.user()) {
+        Counties.update( { _id: tmpl.find('#snowlevels').attr('data-county').value }, 
+          { 
+            name: tmpl.county.value, 
+            snowlevel: tmpl.find('#snowlevels').selectedValue, 
+            reportdate: reportDate, 
+            reportedby: Meteor.user().services.twitter.screenName
+          },
+          { upsert: true }
+        );
+
+      tmpl.find('.reportdate').innerHTML(reportDate);
+      tmpl.find('.reportedby').innerHTML(Meteor.user().services.twitter.screenName);
+      }
+    }
   };
 
-  Template.snowlevels.snowlevels = function() {
-    return Snowlevels.find({}, { sort: ['level', 'desc'] });
-  }
-
 }
-
